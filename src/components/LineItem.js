2f9af2b1fb28f0b8fa2a8fb1f85905bd341d5eab
janrico <janrico@stewleonards.com>
2021-04-08 14:18:34 -0400

    new orders file

--- src/components/LineItem.js
+++ src/components/LineItem.js
@@ -14,17 +14,24 @@ class LineItem {
         if (item.includes('Brownie')) return 'Bakery';
         if (item.includes('Cookie')) return 'Bakery';
         if (item.includes('Baguette')) return 'Bakery';
+        if (item.includes('Fresh Fruit Tart')) return 'Bakery';
         if (item.includes('Peach Blueberry Rustic Tart')) return 'Bakery';
         if (item.includes('Country Hearth Bread')) return 'Bakery';
+        if (item.includes('Rugelach Platter')) return 'Bakery';
         if (item.includes('American PU PU')) return 'BBQ';
-        if (item.includes('Caprese Skewers')) return 'BBQ';
+        if (item.includes('Crispy Fried Chicken')) return 'BBQ';
+        if (item.includes('Boneless Wing Platter')) return 'BBQ';
+        if (item.includes('Wingette Platter')) return 'BBQ';
         if (item.includes('Fresh Mozzarella')) return 'Deli';
         if (item.includes('3 Foot Grinder')) return 'Deli';
         if (item.includes('Wrap Platter')) return 'Deli';
         if (item.includes('Antipasto Platter')) return 'Deli';
         if (item.includes('Cheese and Pepperoni Platter')) return 'Deli';
         if (item.includes('Sandwich Platter')) return 'Deli';
+        if (item.includes('Gourmet Cheese Platter')) return 'Deli';
         if (item.includes('Stuffed Mushroom Platter')) return 'Kitchen';
+        if (item.includes('Mozzarella & Tomato Platter')) return 'Kitchen';
+        if (item.includes('Caprese Skewers')) return 'Kitchen';
         if (item.includes('Butternut Apple Soup')) return 'Kitchen';
         if (item.includes('Pepperoni Stuffed Loaf')) return 'Kitchen';
         if (item.includes('sauce')) return 'Kitchen';
@@ -45,11 +52,12 @@ class LineItem {
         if (item.includes('Ham')) return 'Kitchen';
         if (item.includes('Vegetable Minestrone Soup')) return 'Kitchen';
         if (item.includes('Grilled Salmon')) return 'Kitchen';
+        if (item.includes('Cajun Salmon')) return 'Kitchen'
         if (item.includes('Lobster Bisque')) return 'Kitchen';
         if (item.includes('Butternut Squash')) return 'Kitchen';
         if (item.includes('Roasted Sliced Turkey Breast')) return 'Kitchen';
         if (item.includes('Stuffing')) return 'Kitchen';
-        if ((item.includes('Rugalach')) || item.includes('Rugelach')) return 'Kitchen';
+        if (item.includes('Rugalach')) return 'Kitchen';
         if (item.includes('Roasted Asparagus')) return 'Kitchen';
         if ((item.includes('Sauce')) || item.includes('Gravy')) return 'Kitchen';
         if (item.includes('Macaroni')) return 'Kitchen';
@@ -61,6 +69,8 @@ class LineItem {
         if (item.includes('Cheese Lasagna')) return 'Kitchen';
         if (item.includes('Chicken')) return 'Kitchen';
         if (item.includes('Meatball')) return 'Kitchen';
+        if (item.includes('Au Jus')) return 'Kitchen';
+        if (item.includes('Mint Jelly')) return 'Kitchen';
         if (item.includes('Salad')) return 'Salbar'
         if (item.includes('Fruit')) return 'Produce';
         if (item.includes('Shrimp')) return 'Seafood';
@@ -74,7 +84,6 @@ class LineItem {
         if (item.includes('Smoked Salmon Platter')) return 'Kitchen';
         if (item.includes('Pasta Cruda')) return 'Kitchen';
         if (item.includes('Roasted Yams')) return 'Kitchen';
-        if (item.includes('Kobe Platter')) return 'Kitchen';
         if (item.includes('Tortellini with Creamy Pesto')) return 'Kitchen';
         if (item.includes('Broccoli Cheddar Soup')) return 'Kitchen';
         if (item.includes('Gourmet Cheese Platter')) return 'Kitchen';
@@ -83,7 +92,6 @@ class LineItem {
         if (item.includes("Manhattan Clam Chowder")) return 'Kitchen';
         if (item.includes('Italian Wedding Soup')) return 'Kitchen';
         if (item.includes('Vegetable Platter')) return 'Produce';
-        if (item.includes('California Roll Platter')) return 'Sushi';
         if (item.includes('Eggplant Parmesan')) return 'Kitchen';
         if (item.includes('Eggplant Rollatini')) return 'Kitchen';
         if (item.includes('Vegetable Lasagna')) return 'Kitchen';
@@ -93,8 +101,15 @@ class LineItem {
         if (item.includes("Stew's Naked Turkey - Roasted - Large")) return 'Kitchen';
         if (item.includes("Fukuoka Platter")) return 'Kitchen';
         if (item.includes("Vegetable Lo Mein")) return 'Kitchen';
-
-
+        if (item.includes('California Roll Platter')) return 'Sushi';
+        if (item.includes('Kobe Platter')) return 'Sushi';
+        if (item.includes('Premium Platter')) return 'Sushi';
+        if (item.includes('Sunrise Platter')) return 'Sushi';
+        if (item.includes('Fukuoka Platter')) return 'Sushi';
+        if (item.includes('Spring Platter')) return 'Sushi';
+        if (item.includes('Sausage Stuffed Loaf')) return 'Pizza';
+        if (item.includes('Chicken Parm Stuffed Loaf')) return 'Pizza';
+        if (item.includes('Pepperoni Stuffed Loaf')) return 'Pizza';
     }
 }
 
